2ee550e79c19142de517f3f463adc73e6c8b76f5
Iftekher Aziz <azizchy02@gmail.com>
2023-02-23 03:29:53 +0600

    Commit post.html and post.js

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// Load data dynamically from the API:
+
+/**
+ * It fetches the data from the API and displays it on the page.
+ */
+function loadUser2() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => displayUser2(data));
+}
+/* ----------------------------------------------------------------------- */
+/* 
+function displayUser2(data) {
+  for(const user of data){
+    console.log(user); // this will print the whole object
+    console.log(user.name); // this will print the name of the user
+    console.log(user.email); // this will print the email of the user
+    console.log(user.name,' - ', user.email);
+  }
+} */
+
+/* ----------------------------------------------------------------------- */
+
+/**
+ * It creates a list item for each user in the data array, and appends it to the ul element.
+ * @param data - The data that you want to display.
+ */
+
+function displayUser2(data) {
+    const ul = document.getElementById("users-list");
+    for (const user of data) {
+        const li = document.createElement("li"); // create a li element
+        li.innerText = `Name: ${user.name} -  Email: ${user.email}`; // set the text of the li element
+        ul.appendChild(li); // append the li element to the ul element
+    }
+    }
+
+/* ----------------------------------------------------------------------- */
---
+++ js/post.js
@@ -0,0 +1,53 @@
+/**
+ * It fetches the data from the URL, then converts it to JSON, then passes it to the displayPosts
+ * function.
+ */
+function loadPosts(){
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => displayPosts(data));
+}
+loadPosts();
+
+/* ----------------------------------------------------------------------- */
+
+/**
+ * It loops through the data and prints the title and body of each post.
+ * @param data - This is the data that we are getting from the API.
+ */
+/* function displayPosts(data){
+    for(const post of data){
+        console.log(post); // this will print the whole object
+        console.log(post.title); // this will print the name of the user
+        console.log(post.body); // this will print the email of the user
+        console.log(post.title,' - ', post.body); // this will print the email of the user
+    }
+    } */
+
+/* ----------------------------------------------------------------------- */
+
+/*
+
+How to show the data on the page dynamically: 
+1. Get the container element where you want to display the elements
+2. Create the elements that you want to display
+3. Set the text of the elements
+4. Append the elements to the container element
+
+*/
+
+/* ----------------------------------------------------------------------- */
+
+function displayPosts(data) {
+  const postContainer = document.getElementById("post-container");
+  for (const post of data) {
+    const div = document.createElement("div");
+    div.classList.add("post");
+    div.innerHTML = `
+            <h4 class="post-id">ID: ${post.id}</h4>
+            <h5 class="post-title">Title: ${post.title}</h5>
+            <p class="post-body"><b>Description:</b> ${post.body}</p>
+        `;
+    postContainer.appendChild(div);
+  }
+}
